30df2844d1a5a208a74b214efaf3a9ab284b0897
jwlapinski <lapinskijw@gmail.com>
2020-02-28 20:00:48 -0500

    setup project without storyboards

---
+++ CommitReader/CommitReader/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CommitReader
+//
+//  Created by James Lapinski on 2/28/20.
+//  Copyright © 2020 JWL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+
+
+}
+
